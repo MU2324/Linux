f4faf52ed4c1518e94b9f3259f76f5196780e3cc
Error Exploit <143321616+MU2324@users.noreply.github.com>
2023-12-17 20:37:21 +0530

    Update dns.sh

--- DNS/dns.sh
+++ DNS/dns.sh
@@ -5,6 +5,9 @@ sudo cp db.192 /etc/bind/
 sudo cp named.conf.local /etc/bind/
 sudo cp db.example.com /etc/bind/
 
+sudo systemctl restart bind9
+sudo systemctl status bind9
+
 sudo ufw status 
 #sudo ufw enable
 
